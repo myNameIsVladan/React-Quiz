33edc0c582c14796626754d11cc4d6f22c58f675
myNameIsVladan <vladandjurovic001@gmail.com>
2024-09-12 01:03:33 +0200

    Initial commit

---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import Options from "./Options";
+function Question({ question, dispatch,answer}) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options answer={answer} dispatch={dispatch} question={question} />
+    </div>
+  );
+}
+
+export default Question;
